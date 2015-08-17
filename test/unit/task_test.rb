df06891ca5e0330c790dd0294b1bc60992aff87e
Sohair Ahmad <sohair1991@gmail.com>
2015-08-17 23:17:49 +0500

    TaskTest: Added test case for the validate presence true

--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save task without necessary fields" do
+    task = Task.new
+    assert !task.save
+  end
 end

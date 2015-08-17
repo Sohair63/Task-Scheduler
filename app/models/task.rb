da9f07d8df932280bdc91eedb00a1f778347deef
Sohair Ahmad <sohair1991@gmail.com>
2015-08-17 23:09:21 +0500

    Task: Added model level validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
 
   attr_accessible :name, :executable_path, :start_time, :end_time
 
+  validates :name,            presence: true
+  validates :executable_path, presence: true
+
 end
